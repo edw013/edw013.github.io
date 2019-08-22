45625ee8f29b3509a422292462d375ed69927447
Ethan Wang <edw013@ucsd.edu>
2019-08-22 12:08:10 -0700

    Add spacing in NavBar and body

--- src/App.js
+++ src/App.js
@@ -4,24 +4,40 @@ import Blog, { BlogPost } from "./Blog";
 import { Home } from "./Home";
 import { Navbar, Nav } from "react-bootstrap";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
 class App extends Component {
     render() {
+        const bodyContainer = {
+            "width": "70%",
+            "margin-left": "15%",
+            "margin-right": "15%",
+            "margin-top": "2%"
+        }
+
+        const navContainer = {
+            "width": "70%",
+            "margin-left": "15%",
+            "margin-right": "15%",
+            "display": "flex"
+        }
+
         return (
-            <HashRouter basenmae="/">
+            <HashRouter basename="/">
                 <div>
                     <Navbar bg="light" expand="lg" sticky="top">
-                        <Navbar.Brand>ew.</Navbar.Brand>
-                        <Nav.Link href="#/">Home</Nav.Link>
-                        <Nav.Link href="#/blog">Blog</Nav.Link>
+                        <div style={navContainer}>
+                            <Navbar.Brand>ew.</Navbar.Brand>
+                            <Nav.Link href="#/">Home</Nav.Link>
+                            <Nav.Link href="#/blog">Blog</Nav.Link>
+                        </div>
                     </Navbar>
 
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/blog/" component={Blog} />
-                        <Route exact path="/blog/:id" component={BlogPost} />
-                    </Switch>
+                    <div style={bodyContainer}>
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/blog/" component={Blog} />
+                            <Route exact path="/blog/:id" component={BlogPost} />
+                        </Switch>
+                    </div>
                 </div>
             </HashRouter>
         );
