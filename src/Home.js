f5680bca2edba025a23ad0817ac91090360503c1
Ethan Wang <edw013@ucsd.edu>
2019-09-19 03:39:29 -0400

    Add content

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 
 class Home extends Component {
     render() {
+        let headshotStyle = {
+            "width": "20%"
+        };
+
         return (
             <div>
-                <h1>Ethan Wang's website (heavy WIP)</h1>
-                <h4>unlucky initials tbh</h4>
+                <img src={process.env.PUBLIC_URL + "/ethan.jpg"} alt="Ethan" style={headshotStyle} />
+                <h1>Ethan Wang</h1>
+                <h3>About</h3>
+                <p>I'm a computer science major at UC San Diego.</p>
             </div>
         )
     }
