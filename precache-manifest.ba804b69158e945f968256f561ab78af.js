403baa8db99877c98affc23cc242b6259936ed69
Ethan Wang <edw013@ucsd.edu>
2020-04-08 23:18:25 -0400

    Updates

---
+++ precache-manifest.ba804b69158e945f968256f561ab78af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8b735815b5cdea6b595c5374cf480f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b28d0cb7783ba882c131",
+    "url": "/static/css/main.5cdf8e6f.chunk.css"
+  },
+  {
+    "revision": "1f8f627761deaa7f2be4",
+    "url": "/static/js/2.0a8a55e0.chunk.js"
+  },
+  {
+    "revision": "b28d0cb7783ba882c131",
+    "url": "/static/js/main.9e709425.chunk.js"
+  },
+  {
+    "revision": "26fc66a6d4270cb82f12",
+    "url": "/static/js/runtime~main.279ba5a2.js"
+  }
+]);
